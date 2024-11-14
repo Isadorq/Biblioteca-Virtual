5513ea41e573f73bc2c954b88e1e3fbd5508657a
Isadorq <iscapatice@gmail.com>
2024-11-13 21:31:42 -0300

    em andamento

--- auth-api/app.js
+++ auth-api/app.js
@@ -2,15 +2,9 @@ require('dotenv').config();     // Carrega variáveis de ambiente do arquivo .en
 
 const express = require('express');     // Importa o express, um framework para criar APIs
 const mongoose = require('mongoose');   // Importa o mongoose para conectar e interagir com o MongoDB
-const cors = require('cors');    // Importa o middleware CORS
 
 const app = express();  // Inicializa uma aplicação express
 const authRotes = require('./routes/authRoutes')    // Importa as rotas de autenticação (ainda a serem criadas)
-
-app.use(cors({
-    origin: 'http://localhost:8080',        // Permite requisições apenas do seu frontend
-    methods: ['GET', 'POST'],       // Adicione outros métodos se necessário
-}));
 
 app.use(express.json());    // Middleware para processar as requisições JSON
 
@@ -27,5 +21,4 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((error) => console.log('Erro ao conecta ao MongoDB', error));    // Se falhar, exibe mensagem de erro
 
 const PORT = process.env.PORT || 5000;      // Define a porta do servidor, usando variavel de ambiente ou padrão 5000
-
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));   // Inicia o servidor e exibe mensagem no console
